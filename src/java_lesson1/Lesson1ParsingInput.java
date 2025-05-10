41d08ee755e097383b9138687bf47af835f181b4
ChaosCedd <166236276+icodecedd@users.noreply.github.com>
2025-05-10 23:18:29 +0800

    Add sample code demonstrating parsing and Scanner usage in Java

---
+++ src/java_lesson1/Lesson1ParsingInput.java
@@ -0,0 +1,19 @@
+package java_lesson1;
+
+public class Lesson1ParsingInput {
+	
+	public static void main(String[] args) {
+		String firstname, lastname;
+	    int age;
+	    double weight;
+        firstname = args[0];
+	    lastname = args[1];
+	    age = Integer.parseInt(args[2]);
+	    weight = Double.parseDouble(args[3]);
+		System.out.println("Your Name is: " + firstname + " " + lastname);
+	    System.out.println("Your Age is: " + age);
+	    System.out.println("Your Weight is: " + weight);
+
+	}
+
+}
